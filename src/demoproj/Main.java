c290c59c6b658ddee82465753e7705306a6fcf34
Ahmed Elbashir <ahmed.a.bashir@gmail.com>
2019-03-11 22:43:15 -0600

    Initial commit.

---
+++ src/demoproj/Main.java
@@ -0,0 +1,41 @@
+package demoproj;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double coolValue = 12.134;
+		int randValue = 13;
+		
+		String username = null;
+		
+		System.out.println("Cool Value is");
+		
+		System.out.println("Your name has " + username.length() + " characters");
+		
+		func("Hello, ", username);
+		
+		boolean isOkay = false;
+		
+		if(isOkay = true) {
+			System.out.println("It's okay.");
+		} else {
+			// ..
+		}
+		
+		
+	}
+	
+	
+	
+	public static void func(String str1, String str2) {
+		System.out.println("Str1 has " + str1.length() + " characters.");
+		System.out.println("Str2 has " + str2.length() + " characters.");
+		System.out.println(str1 + str2);
+	}
+	
+
+
+}
+
+
